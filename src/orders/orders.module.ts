83c1ed8593b4873acff119ba5103860855a7aca5
Szymon Półtorak <poltorak.szymon@gmail.com>
2019-09-27 23:41:20 +0200

    Add slack notification

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderRepository } from './order.repository';
 import { VendorRepository } from '../vendors/vendor.repository';
 import { AuthModule } from '../auth/auth.module';
+import { SlackModule } from '../slack/slack.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([OrderRepository, VendorRepository]),
-    AuthModule
+    AuthModule,
+    SlackModule,
   ],
   controllers: [OrdersController],
   providers: [OrdersService]
