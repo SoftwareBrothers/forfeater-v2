ba29d456e03c31b87f526bc299d73689c3e1c09d
Szymon Półtorak <poltorak.szymon@gmail.com>
2019-09-27 10:26:27 +0200

    Add editorconfig

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,7 +3,7 @@ import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.providers';
 
 @Module({
-    providers: [databaseProviders],
-    exports: [databaseProviders],
+  providers: [databaseProviders],
+  exports: [databaseProviders],
 })
 export class DatabaseModule { }
